fbc4c111c0d40ecee2bf01b801a64e8faa287f99
ignzdave <david.sutanto@live.com>
2019-05-04 13:21:57 +0800

    change http to https

--- phpQS.php
+++ phpQS.php
@@ -177,7 +177,7 @@ function debug_to_console( $data ) {
 
 <head>
     <title>Analyze Sample</title>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 </head>
 
 <body>
